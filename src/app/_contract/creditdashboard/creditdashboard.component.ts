55adc933339fd75c1697de06b083f6a4b2f98f4f
naresh <naresh101010@gmail.com>
2020-09-07 21:48:12 +0530

    u

--- src/app/_contract/creditdashboard/creditdashboard.component.ts
+++ src/app/_contract/creditdashboard/creditdashboard.component.ts
@@ -441,7 +441,10 @@ export class CreditdashboardComponent implements OnInit {
         else{
         favoriteObj.menuCard = true;
         }
-        isFavourite !==undefined ? isFavourite['favoriteIcon'] = 'favorite_border' : null;
+        // isFavourite !==undefined ? isFavourite['favoriteIcon'] = 'favorite_border' : '';
+         if (isFavourite !== undefined) {
+           isFavourite["favoriteIcon"] = "favorite_border";
+         } 
         //isFavourite['favoriteIcon'] = isFavourite !==undefined ? 'favorite_border' : '';
         favoriteObj.objectOrder = index+1;
         favoriteObj.status = isFavourite !== undefined ? 0 : 1;
